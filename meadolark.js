b56d36692379ed0a3ed8c76580772c6c0ad3ede7
Pavel Solovev <pashius2010@gmail.com>
2021-05-27 23:34:06 +0400

    entry point

---
+++ meadolark.js
@@ -0,0 +1,35 @@
+const express = express("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send("Meadowalrk Travel");
+});
+
+app.get("/about", (req, res) => {
+  res.type("text/plain");
+  res.send("O Meadowlark Travel");
+});
+
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Не найдено");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 - Ошибка сервера");
+});
+
+app.listen(port, () =>
+  console.log(
+    `Express запущен на http://localhost:${port}; ` +
+      `нажмите Ctrl+C для завершения.`
+  )
+);
