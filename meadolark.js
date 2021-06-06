b1e5f6d1efd2fb55e22854a2945cde4cfc818fab
Pavel Solovev <pashius2010@gmail.com>
2021-06-06 18:17:27 +0400

    templates partials

--- meadolark.js
+++ meadolark.js
@@ -1,36 +1,44 @@
-const express = require("express");
-const expressHandlebars = require("express-handlebars");
-const handlers = require("./lib/handlers");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const handlers = require('./lib/handlers');
 
 const app = express();
-
 app.engine(
-  "handlebars",
-  expressHandlebars({
-    defaultLayout: "main",
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      section: function (name, options) {
+        if (!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+      },
+    },
   })
 );
-app.set("view engine", "handlebars");
+
+app.set('view engine', '.hbs');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/", handlers.home);
+app.get('/', handlers.home);
 
-app.get("/about", handlers.about);
+app.get('/about', handlers.about);
 
-app.use(handlers.notFount);
+// app.use(handlers.notFount);
 
-app.use(handlers.serverError);
+// app.use(handlers.serverError);
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(
       `Express запущен на http://localhost:${port}` +
-        "; нажмите Ctrl + C для завершения."
+        '; нажмите Ctrl + C для завершения.'
     );
   });
 } else {
-  module.exports = appp;
+  module.exports = app;
 }
